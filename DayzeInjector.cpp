6ea9c4bb64ab0b0c9cd72ef8e6351c547bc8d956
azfasty <143274420+azfasty@users.noreply.github.com>
2025-02-17 10:42:08 +0100

    Update DayzeInjector.cpp

--- DayzeInjector.cpp
+++ DayzeInjector.cpp
@@ -1,7 +1,59 @@
+// Fichier : DayzeInjector.cpp
+
 #include <windows.h>
 #include "imgui.h"
 #include "Functions.h"
 
+// Déclaration de la fonction ShowDayzeMenu()
+void ShowDayzeMenu();
+
+// Fonction principale pour une application Windows GUI
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    // Création d'une fenêtre Windows (exemple basique)
+    WNDCLASS wc = { 0 };
+    wc.lpfnWndProc = DefWindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = "DayzeInjectorClass";
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindowEx(0, "DayzeInjectorClass", "Dayze Injector", WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 800, 600, NULL, NULL, hInstance, NULL);
+
+    if (!hwnd) return 0;
+
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
+
+    // Initialisation ImGui
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; 
+
+    // Boucle principale Windows
+    MSG msg;
+    bool running = true;
+    while (running) {
+        while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+            if (msg.message == WM_QUIT) {
+                running = false;
+            }
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+
+        // Démarrage de la frame ImGui
+        ImGui::NewFrame();
+        ShowDayzeMenu(); // Affichage du menu
+        ImGui::Render();
+    }
+
+    // Nettoyage ImGui
+    ImGui::DestroyContext();
+    return 0;
+}
+
+// Affichage du menu ImGui
 void ShowDayzeMenu() {
     ImGui::Begin("Dayze - GTA V / FiveM Cheat");
 
